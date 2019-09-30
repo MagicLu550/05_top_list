eafdc779d08df881a2df9fe60fc0ad3a1081524f
magiclu550 <843983728@qq.com>
2019-09-30 19:10:43 +0800

    添加了Controller

--- src/main/java/net/noyark/top/Application.java
+++ src/main/java/net/noyark/top/Application.java
@@ -5,8 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //目前还差
-//1. 上传图片 删除图片 添加top的控制器和页面
+//1. 上传图片 删除图片 添加top 删除top的控制器和页面
 //2. 底层的mapper没有完成
+//3. 美化图片显示
 @SpringBootApplication
 @MapperScan(basePackages = "net.noyark.top.mapper")
 public class Application {
